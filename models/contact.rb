59c1d5a0c707c83b5b2c6fc218dccc9b4209689f
Alexandre Stanescot <alex@aerostan.com>
2022-12-27 17:32:41 +0100

    Contact category as enum because of translations

--- models/contact.rb
+++ models/contact.rb
@@ -2,6 +2,10 @@ class Contact < ApplicationRecord
   CATEGORIES = [  I18n.t('activerecord.attributes.contact.categories.quotation'),
                   I18n.t('activerecord.attributes.contact.categories.invoicing_problem'),
                   I18n.t('activerecord.attributes.contact.categories.other') ]
+  enum :category, { CATEGORIES[0] => 0,
+                    CATEGORIES[1] => 10,
+                    CATEGORIES[2] => 20 
+                  } # OPTIMIZE: Improve this sentence, how to get ride of CATEGORIES constant?
 
   validates :email, presence: true
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
--- template.rb
+++ template.rb
@@ -137,7 +137,7 @@ after_bundle do
   generate(:migration, "ChangeSignInCountToUser",  "sign_in_count:string")
 
   # contact.rb
-  generate(:model, "contact", "last_name:string", "first_name:string", "company:string", "email:string", "phone:string", "category:string", "description:text", "accept_private_data_policy:boolean", "active:boolean")
+  generate(:model, "contact", "last_name:string", "first_name:string", "company:string", "email:string", "phone:string", "category:integer", "description:text", "accept_private_data_policy:boolean", "active:boolean")
 
   # Loading all models
   run 'rm app/models/user.rb'
