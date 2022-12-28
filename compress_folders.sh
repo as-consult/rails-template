6355dd51172d5a22a188bc2e4103adb5f1654dd6
Alexandre Stanescot <alex@aerostan.com>
2022-12-28 11:00:51 +0100

    Fix: Compress folders ignores .swp files

--- compress_folders.sh
+++ compress_folders.sh
@@ -41,61 +41,61 @@ if [ -e helpers.tar.gz ]
 fi
 echo -e "---"
 echo -e "Creating compressed forlders:"
-tar -czf stylesheets.tar.gz stylesheets
+tar -czf stylesheets.tar.gz --exclude="*.swp" stylesheets
 if [ -e stylesheets.tar.gz ]
   then echo "✅ stylesheets.tar.gz created successfully"
 else
   echo "⛔ Error for stylesheets.tar.gz"
 fi
-tar -czf javascript.tar.gz javascript
+tar -czf javascript.tar.gz --exclude="*.swp" javascript
 if [ -e javascript.tar.gz ]
   then echo "✅ javascript.tar.gz created successfully"
 else
   echo "⛔ Error for javascript.tar.gz"
 fi
-tar -czf images.tar.gz images
+tar -czf images.tar.gz --exclude="*.swp" images
 if [ -e images.tar.gz ]
   then echo "✅ images.tar.gz created successfully"
 else
   echo "⛔ Error for images.tar.gz"
 fi
-tar -czf views.tar.gz views
+tar -czf views.tar.gz --exclude="*.swp" views
 if [ -e views.tar.gz ]
   then echo "✅ views.tar.gz created successfully"
 else
   echo "⛔ Error for views.tar.gz"
 fi
-tar -czf models.tar.gz models
+tar -czf models.tar.gz --exclude="*.swp" models
 if [ -e models.tar.gz ]
   then echo "✅ models.tar.gz created successfully"
 else
   echo "⛔ Error for models.tar.gz"
 fi
-tar -czf controllers.tar.gz controllers
+tar -czf controllers.tar.gz --exclude="*.swp" controllers
 if [ -e controllers.tar.gz ]
   then echo "✅ controllers.tar.gz created successfully"
 else
   echo "⛔ Error for controllers.tar.gz"
 fi
-tar -czf mailers.tar.gz mailers
+tar -czf mailers.tar.gz --exclude="*.swp" mailers
 if [ -e mailers.tar.gz ]
   then echo "✅ mailers.tar.gz created successfully"
 else
   echo "⛔ Error for mailers.tar.gz"
 fi
-tar -czf db.tar.gz db
+tar -czf db.tar.gz --exclude="*.swp" db
 if [ -e db.tar.gz ]
   then echo "✅ db.tar.gz created successfully"
 else
   echo "⛔ Error for db.tar.gz"
 fi
-tar -czf config.tar.gz config
+tar -czf config.tar.gz --exclude="*.swp" config
 if [ -e config.tar.gz ]
   then echo "✅ config.tar.gz created successfully"
 else
   echo "⛔ Error for config.tar.gz"
 fi
-tar -czf helpers.tar.gz helpers
+tar -czf helpers.tar.gz --exclude="*.swp" helpers
 if [ -e helpers.tar.gz ]
   then echo "✅ helpers.tar.gz created successfully"
 else
