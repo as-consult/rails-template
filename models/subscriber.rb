e7683635186223fb9043cec305b56caa80e2e432
Alexandre Stanescot <alex@aerostan.com>
2023-01-07 07:59:02 +0100

    Fix: model missing for newsletter unsubscribe

--- models/subscriber.rb
+++ models/subscriber.rb
@@ -1,5 +1,13 @@
 class Subscriber < ApplicationRecord
+  before_create :add_unsubscribe_hash
+
   validates :name, :email, presence: true
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   validates_inclusion_of :accept_private_data_policy, in: [ true ]
+
+  private
+
+  def add_unsubscribe_hash
+    self.unsubscribe_hash = SecureRandom.hex
+  end
 end
